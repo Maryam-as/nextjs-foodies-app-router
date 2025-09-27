d05dab734e2e5af684490da45e952b0327afc48c
Maryam-as <203218399+Maryam-as@users.noreply.github.com>
2025-09-27 16:24:14 +0330

    enhance ImagePicker with custom button trigger and client-side logic
    - Add 'use client' directive to enable hooks in ImagePicker
    - Hide file input by adding 'input' class
    - Add custom <button> element to trigger file selection
    - Implement handlePickClick with useRef to programmatically click hidden input

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,16 +1,35 @@
+"use client";
+
+import { useRef } from "react";
+
 import classes from "./image-picker.module.css";
 
 export default function ImagePicker({ label, name }) {
+  const imageInputRef = useRef();
+
+  function handlePickClick() {
+    imageInputRef.current.click();
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className="classes.controls">
         <input
+          ref={imageInputRef}
+          className={classes.input}
           type="file"
           id={name}
           name={name}
           accept="image/png, image/jpeg"
         />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
       </div>
     </div>
   );
